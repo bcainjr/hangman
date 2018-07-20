5e89483ec274841931abf8f84d6bdf0a6ac6c877
Bruce Cain <brucecainjr@gmail.com>
2018-07-20 08:26:14 -0400

    Able to accept and handle file on command line

--- hangman.c
+++ hangman.c
@@ -15,58 +15,37 @@
 #define FILEHANGMAN ".hangman"
 
 /* Plays the game loop */
-void hangmanGame(char const *word);
+void hangmanGame(char *word);
+/* Determine file to use and grab word to use in game */
+char *readFile(int const argc, char *argv[]);
 /* Count number of lines in a file */
-char countLines(FILE *pFile);
+char countLines(void);
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    FILE *pWords;
-    char *input = NULL;
-    int lines, randLine;
-    size_t size;
+    char *fileInput;
 
-    srand(time(NULL));
 
-    pWords = fopen(FILEWORDS, "r");
-
-    lines = countLines(pWords);
-    printf("lines %d\n", lines);
-    
-    randLine = (rand() % lines);
-    printf("%d\n", randLine);
-
-    if (pWords != NULL)
-    {
-        for (int i = 0;  i < randLine; i++)
-        {
-            printf("%d\n", i);
-            getline(&input, &size, pWords);
-            input[strlen(input) - 1] = '\0';
-            printf("%s\n", input);
-        }
-    }
-
-    fclose(pWords);
+    fileInput = readFile(argc, argv);
 
     /* Validate word */
 
-    printf("%lu\n", strlen(input));
-    if (strlen(input) <= 34)
+    printf("%s\n", fileInput);
+    if (strlen(fileInput) <= 34)
     {
-        hangmanGame(input);
+        hangmanGame(fileInput);
     }
 
-    free(input);
     return 0;
 }
 
-void hangmanGame(char const *word)
+void hangmanGame(char *word)
 {
     int games = 0, wins = 0, loss = 0, playing = 1, misses = 0, hits = 0;
     unsigned long  wordLength = strlen(word);
     char *input = NULL, *mark, found;
     double average = 0.0;
+    /* size_t is unsigned int */
     size_t size;
 
     mark = (char *) malloc(wordLength);
@@ -114,7 +93,7 @@ void hangmanGame(char const *word)
         }
 
         /* Check if player lost */
-        if (misses > 6)
+        if (misses == 5)
         {
             printf("%s\nYou Lost...\n", word);
             playing = 0;
@@ -133,16 +112,81 @@ void hangmanGame(char const *word)
     free(input);
 }
 
-char countLines(FILE *pFile)
+char countLines(void)
 {
-    char lines = 0;
-    char *line = NULL;
+    FILE *pWords;
+    char lines = 0, *line = NULL;
     size_t size;
 
-    while (getline(&line, &size, pFile) != -1)
+    pWords = fopen(FILEWORDS, "r");
+
+    while (getline(&line, &size, pWords) != -1)
     {
         lines++;
     }
 
+    fclose(pWords);
+
     return lines;
+}
+
+char *readFile(int const argc, char *argv[])
+{
+    FILE *pFile;
+    char *input = NULL, opened = 0;
+    size_t size;
+    int randLine, lines;
+
+    srand(time(NULL));
+
+    if (argc > 2)
+    {
+        printf("Too many Command Line arguments.\n");
+        printf("Enter file name to word list only.\n");
+        /* Exit with 1 for too many CLI arguments */
+        exit(1);
+    }
+
+    if (argc == 2)
+    {
+        pFile = fopen(argv[1], "r");
+        printf("Enter first file open try\n");
+        if (pFile)
+        {
+            opened = 1;
+        }
+    }
+    
+    if (!opened)
+    {
+        pFile = fopen(FILEWORDS, "r");
+
+        if (pFile == NULL)
+        {
+            printf("No file could be opened.\n");
+            /* Could not open any file to read words */
+            exit(2);
+        }
+    }
+
+    lines = countLines();
+    printf("lines %d\n", lines);
+    
+    randLine = (rand() % lines) + 1;
+    printf("%d\n", randLine);
+
+    if (pFile != NULL)
+    {
+        for (int i = 0; i < randLine; i++)
+        {
+            getline(&input, &size, pFile);
+            input[strlen(input) - 1] = '\0';
+            printf("%s\n", input);
+
+            /* Validate word if fail quit? */
+        }
+    }
+
+    fclose(pFile);
+    return input;
 }
